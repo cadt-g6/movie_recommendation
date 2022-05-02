e35941497c1969c32b86ccf8d97dfa9408e8ceb0
Thea Choem <theacheng.g6@gmail.com>
2022-05-02 10:55:54 +0700

    close #4 access dataset with enum

---
+++ lib/types/dataset_type.py
@@ -0,0 +1,24 @@
+from enum import Enum
+
+
+class DatasetType(Enum):
+    credit = "credits.csv"
+    keywords = "keywords.csv"
+    links_small = "links_small.csv"
+    links = "links.csv"
+    movies_metadata = "movies_metadata.csv"
+    ratings_small = "ratings_small.csv"
+    ratings = "ratings.csv"
+
+    def sample_path(self, prefix: str = "..") -> str:
+        return prefix + "/" + "datasets/samples" + "/" + self.value
+
+    def original_path(self, prefix: str = "..") -> str:
+        return prefix + "/" +"datasets/original" + "/" + self.value
+
+    # source: SourceType
+    def path(self, source, prefix: str = "..") -> str:
+        return prefix + "/" + source.value + "/" + self.value
+
+    def values() -> list:
+        return [path for path in DatasetType]
