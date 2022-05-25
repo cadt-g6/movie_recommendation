2d14cfec31fcea53eacb5372760ed8fc6da8cd0d
VatanakChamroeun <ch.vatanak01@gmail.com>
2022-05-25 22:58:23 +0700

    fix: return top 10 movies title

--- lib/services/top_10_movies_analytic.py
+++ lib/services/top_10_movies_analytic.py
@@ -6,32 +6,32 @@ import pandas as pd
 
 
 class Top10MoviesAnalystic:
-    def __init__(self) -> None:
-        self.movie_df = pd.read_csv(
-            DatasetType.movies_metadata.sample_path())
+    def __init__(self, data):
+        self.movie_df = data
 
     def top_10_based_on_popularity(self):
-        top_popular_movies = self.movie_df.sort_values(
+        top_popular_movies = self.movie_df[:31000].sort_values(
             by=['popularity'], ascending=False)
-        return top_popular_movies.head(10)
+        return top_popular_movies.head(1)['title'].to_list()
 
     def top_10_based_on_rating(self):
-        top_avg_quan_movies = self.movie_df.sort_values(
+        top_avg_quan_movies = self.movie_df[:31000].sort_values(
             by=['avg_rating', 'quantity'], ascending=False)
-        return top_avg_quan_movies.head(10)
+        return top_avg_quan_movies.head(10)['title'].to_list()
 
     def top_10_based_on_revenue(self):
-        top_revenue_movies = self.movie_df.sort_values(
+        top_revenue_movies = self.movie_df[:31000].sort_values(
             by=['revenue'], ascending=False)
 
-        return top_revenue_movies.head(10)
+        return top_revenue_movies.head(10)['title'].to_list()
 
     def top_10_based_on_profit(self):
-        cleaned_budget_df = self.movie_df[self.movie_df["budget"] > 5000]
+        cleaned_budget_df = self.movie_df[:
+                                          31000][self.movie_df["budget"] > 5000]
         cleaned_revenue_df = cleaned_budget_df[cleaned_budget_df['revenue'] != 0]
 
         profit_percentage = cleaned_revenue_df.assign(
             profit=lambda row: ((row['revenue'] - row['budget'])/row['budget'])*100)
 
         return profit_percentage.sort_values(
-            by=['profit'], ascending=False).head(10)
+            by=['profit'], ascending=False).head(10)['title'].to_list()
